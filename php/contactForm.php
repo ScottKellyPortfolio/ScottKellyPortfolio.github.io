cd0f7234b8137881439ea351c6529957c790d0d9
Scott Kelly <sctkelly@gmail.com>
2016-07-26 14:20:23 -0400

    New portfolio added!

---
+++ php/contactForm.php
@@ -0,0 +1,7 @@
+<?php
+
+// Insert your SMTP code or mail() function here.
+// You can take a look at this if you need instructions: 
+// http://stackoverflow.com/questions/14456673/sending-email-with-php-from-an-smtp-server
+
+?>
